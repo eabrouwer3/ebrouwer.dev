7cce4fb305320b9f55fde216946d37fce295ab18
Ethan Brouwer <1647525+eabrouwer3@users.noreply.github.com>
2023-12-30 14:49:00 -0700

    Fix other network reference

--- main.tf
+++ main.tf
@@ -211,7 +211,7 @@ resource "google_compute_instance" "hc9-vm" {
 
 resource "google_compute_firewall" "hc9-http-access" {
   name    = "${local.hc9_instance_name}-http"
-  network = google_compute_network.hc9-network.id
+  network = "default"
 
   allow {
     protocol = "tcp"
